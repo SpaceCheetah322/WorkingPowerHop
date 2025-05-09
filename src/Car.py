63b694159a2d6f48c6d35590be71d696f5eb8417
KaiHale16 <155692145+SpaceCheetah322@users.noreply.github.com>
2025-05-09 12:31:09 -0600

    Update Car.py

--- src/Car.py
+++ src/Car.py
@@ -7,21 +7,13 @@ class Car:
         self.speed = speed
         self.vehicle_type = vehicle_type
 
+        image_file = "Car.png" if self.vehicle_type == "car" else "Truck.png"
+        self.image = loadImage(image_file)
 
-        image_file = "Car.png"
-        if self.vehicle_type == "truck":
-            image_file = "Truck.png"
-
-        self.image = Image(image_file)
-        
-
-        scale = 0.2 if self.vehicle_type == "car" else 0.3
-        self.image.set_size(self.image.get_width() * scale, self.image.get_height() * scale)
-        self.image.set_position(self.x, self.y)
-        add(self.image)
-
-        self.width = self.image.get_width()
-        self.height = self.image.get_height()
+        scale = 1 if self.vehicle_type == "car" else 0.3
+        self.width = self.image.width * scale
+        self.height = self.image.height * scale
+        self.image.resize(int(self.width), int(self.height))
 
     def move(self):
         if self.direction == 'right':
@@ -33,17 +25,11 @@ class Car:
             if self.x + self.width < 0:
                 self.x = self.start_x
 
-        self.image.set_position(self.x, self.y)
+    def display(self):
+        image(self.image, self.x, self.y)
 
     def check_collision(self, frog):
-        if (frog.x < self.x + self.width and
-            frog.x + frog.width > self.x and
-            frog.y < self.y + self.height and
-            frog.y + frog.height > self.y):
-            return True
-        return False
-
-    def display(self):
-        if self.image not in get_elements():
-            add(self.image)
-        self.image.set_position(self.x, self.y)
+        return (frog.x < self.x + self.width and
+                frog.x + frog.width > self.x and
+                frog.y < self.y + self.height and
+                frog.y + frog.height > self.y)
